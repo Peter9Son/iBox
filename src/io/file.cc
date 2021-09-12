e6688744b97c217468990f8247479cfc721a6dd2
Peter9Son <peter9.son@gmail.com>
2021-09-12 10:06:32 -0700

    2021091203

--- src/io/file.cc
+++ src/io/file.cc
@@ -1,9 +1,28 @@
-#include <sys/stat.h>
+#include <stdio.h> 
+#include <sys/types.h> 
+#include <sys/stat.h> 
+#include <unistd.h> 
+#include <errno.h> 
+#include <string.h> 
+#include <fcntl.h>
+
+#include <string>
 
 #include "io/file.hpp"
+
+fd_t file_open(char* path, mode_t mode){
+    fd_t fd;
+    fd = open(path, mode);
+    return fd;
+}
+
+int file_close(fd_t fd){
+    return close(fd);
+}
 
 int64_t get_file_stat(fd_t fd){
     struct stat buffer;
     int result = fstat(fd, &buffer);
     return buffer.st_size;
 }
+
