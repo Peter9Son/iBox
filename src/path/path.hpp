aae3c2cd366eee7cca0866576b0848fe29528929
Peter9Son <peter9.son@gmail.com>
2021-09-20 21:29:55 -0700

    20210920

--- src/path/path.hpp
+++ src/path/path.hpp
@@ -1,11 +1,12 @@
 #include <string>
 #include "io/directory.hpp"o
+#include "io/file.hpp"o
 
 #define DIR_LENGTH 3
 
-class pathstring : public directoryEntity {
+class pathstring : public directoryEntity, fileEntity {
 public:
-    pathstring() : directoryEntity() {};
+    pathstring() : directoryEntity(), fileEntity() {};
     ~pathstring() {};
     std::string getpath(std::string name){};
     std::string setpath (std::string bucket, std::string name) {};
