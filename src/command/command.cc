eeed55a9be35a5d6162119f9e7b45892773ce50c
Peter9Son <peter9.son@gmail.com>
2021-09-26 16:09:41 -0700

    2021092602

--- src/command/command.cc
+++ src/command/command.cc
@@ -1,15 +1,13 @@
 #include "command/command.hpp"
 
-int command_file_create(std::string bucket, std::string name){
-    std::string test = get_pa
+std::string command_file_create(std::string bucket, std::string name){
+};
+std::string command_file_write(std::string bucket, std::string name){
 
 };
-int command_file_write(std::string bucket, std::string name){
+std::string command_file_read(std::string bucket, std::string name){
 
 };
-int command_file_read(std::string bucket, std::string name){
-
-};
-int command_file_delete(std::string bucket, std::string name){
+std::string command_file_delete(std::string bucket, std::string name){
 
 };
