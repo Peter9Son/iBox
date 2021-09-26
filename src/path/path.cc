4c559bfd21bba335399a80fc826f17e8bee331bf
Peter9Son <peter9.son@gmail.com>
2021-09-26 11:56:46 -0700

    2021092602

--- src/path/path.cc
+++ src/path/path.cc
@@ -9,11 +9,14 @@
 std::string set_bucket (std::string bucket) {
     if (bucket.empty()){
         perror("string empty");
+        return NULL;
     } else {
        DirectoryEntity dir;
        if (dir.dir_open(bucket) == NULL){
-           if (dir.dir_create(bucket, 0777) < 0)
+           if (dir.dir_create(bucket, 0777) < 0) {
                     perror("faile in dir creation");
+                    return NULL;
+           }
             return bucket;
        }
     }
@@ -23,6 +26,7 @@ std::string set_bucket (std::string bucket) {
 std::string get_path(std::string bucket, std::string name){
     if (name.empty()){
         perror("string empty");
+        return NULL;
     } else {
         if (file_name_lengh(name) <= DIR_LENGTH){
             return bucket + "/" + DEFAULT_DIR_NAME + "/" + name;
@@ -34,21 +38,30 @@ std::string get_path(std::string bucket, std::string name){
 
 std::string set__path (std::string bucket, std::string name) {
     if (name.empty()){
-        perror("string empty");
+        perror("file name string empty");
+        return NULL;
     } else {
         DirectoryEntity dir;
+        if (dir.dir_open(bucket) == NULL){
+            perror("bucket string empty");
+            return NULL;
+        }
         if (file_name_lengh(name) <= DIR_LENGTH){
             if (dir.dir_open(bucket + "/" + DEFAULT_DIR_NAME) == NULL){
                 chdir((bucket + "/" ).c_str());
-                if (dir.dir_create(DEFAULT_DIR_NAME, 0777) < 0)
+                if (dir.dir_create(DEFAULT_DIR_NAME, 0777) < 0) {
                     perror("faile in dir creation");
+                    return NULL;
+                }
             }
             return  bucket + "/" + DEFAULT_DIR_NAME + "/" + name;
         } else{
             if (dir.dir_open(bucket + "/" + name) == NULL){
                 chdir((bucket + "/" ).c_str());
-                if (dir.dir_create(name.substr(0,DIR_LENGTH), 0777) < 0)
+                if (dir.dir_create(name.substr(0,DIR_LENGTH), 0777) < 0) {
                     perror("faile in dir creation");
+                    return NULL;
+                }
             }
             return bucket + "/" + name.substr(0,DIR_LENGTH) + "/" + name;
         }
